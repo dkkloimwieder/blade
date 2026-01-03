327897c7593aa1bb2757d35d469325876ecbec7a
dkk <dkkloimwieder@gmail.com>
2026-01-03 17:52:14 -0500

    run-wasm: Add CSS for fullscreen canvas
    Makes canvas fill viewport for WASM examples.
    
    --kaalin

--- run-wasm/src/main.rs
+++ run-wasm/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    cargo_run_wasm::run_wasm_with_css("body { margin: 0px; }");
+    cargo_run_wasm::run_wasm_with_css(
+        "body { margin: 0px; } \
+         canvas { width: 100vw; height: 100vh; display: block; }",
+    );
 }
